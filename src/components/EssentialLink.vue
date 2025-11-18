bbb9e46df823c4bf2443d3a59589bb072709adfc
Gift <aldreigiftf@gmail.com>
2025-11-18 13:35:30 +0800

    Starter Pack

--- src/components/EssentialLink.vue
+++ src/components/EssentialLink.vue
@@ -11,17 +11,4 @@
   </q-item>
 </template>
 
-<script setup lang="ts">
-export interface EssentialLinkProps {
-  title: string;
-  caption?: string;
-  link?: string;
-  icon?: string;
-}
-
-withDefaults(defineProps<EssentialLinkProps>(), {
-  caption: '',
-  link: '#',
-  icon: '',
-});
-</script>
+<script setup lang="ts"></script>
