1c7e2ad660308c18483b3fcb978661c2ce304fd9
Gift <aldreigiftf@gmail.com>
2025-11-18 16:20:10 +0800

    TAGUM CITY LOCAL SYSTEM

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -4,7 +4,15 @@
       <q-toolbar>
         <q-btn flat dense round icon="menu" aria-label="Menu" @click="toggleLeftDrawer" />
 
-        <q-toolbar-title> TAGUM CITY LOCAL SYSTEM </q-toolbar-title>
+        <q-toolbar-title>
+          <img
+            src="../assets/logo.svg"
+            srcset="../assets/logo.svg 2x"
+            alt="Tagum City Local System Logo"
+            style="height: 32px; width: auto; margin-right: 8px; vertical-align: middle"
+          />
+          TAGUM CITY LOCAL SYSTEM
+        </q-toolbar-title>
       </q-toolbar>
     </q-header>
 
@@ -17,6 +25,12 @@
     <q-page-container>
       <router-view />
     </q-page-container>
+
+    <q-footer elevated>
+      <q-toolbar>
+        <q-toolbar-title align="center"> © 2025 Tagum City Local System </q-toolbar-title>
+      </q-toolbar>
+    </q-footer>
   </q-layout>
 </template>
 
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,10 +1,50 @@
 <template>
-  <q-page class="row items-center justify-evenly">
-    <div grid="col-12 col-md-8">
-      <div></div>
-      <div></div>
-    </div>
-  </q-page>
-</template>
+  <div class="row justify-evenly q-pa-md q-mt-xl">
+    <div class="q-pa-md row items-start">
+      <q-card class="my-card">
+        <img class="Img" src="https://i.imgur.com/cZ9xDdi.jpeg" />
 
-<script setup lang="ts"></script>
+        <q-card-section>
+          <div class="text-h6">Local System 1</div>
+          <div class="text-subtitle2">by Morre</div>
+        </q-card-section>
+      </q-card>
+    </div>
+
+    <div class="q-pa-md row items-start">
+      <q-card class="my-card">
+        <img class="Img" src="https://i.imgur.com/uLE4dLk.png" />
+
+        <q-card-section>
+          <div class="text-h6">Local System 2</div>
+          <div class="text-subtitle2">by Musong</div>
+        </q-card-section>
+      </q-card>
+    </div>
+
+    <div class="q-pa-md row items-start">
+      <q-card class="my-card">
+        <img class="Img" src="https://i.imgur.com/mbUPjsA.png" />
+
+        <q-card-section>
+          <div class="text-h6">Local System 3</div>
+          <div class="text-subtitle2">by Maedel</div>
+        </q-card-section>
+      </q-card>
+    </div>
+  </div>
+</template>
+<script lang="ts"></script>
+
+<style lang="sass" scoped>
+.my-card
+  width: 100%
+  max-width: 250px
+  transition: transform 0.2s
+  &:hover
+    transform: scale(1.05)
+.Img
+  width: 400px
+  height: 300px
+  object-fit: fit
+</style>
